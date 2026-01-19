46fb41b850440ef59bad0227a5cc6e71e80e4ec5
Yurin <0210yurin@gmail.com>
2026-01-20 08:32:46 +0900

    複数ユーザー残高管理

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,3 +4,12 @@
 
 * Add: DEPOSIT amount
 * Add: BALANCE
+
+## v0.2.0
+
+* Add: 複数ユーザー残高管理（dictによる管理）
+* Change: 未登録ユーザーの残高は 0 とみなす仕様に対応
+* Add: `DEPOSIT user amount` コマンド
+* Add: `BALANCE user` コマンド
+* Fix: 未登録ユーザー参照時の KeyError を防止
+
--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 import sys
 
 lines = sys.stdin.read().splitlines() #["DEPOSIT 100", "BALANCE"]
-balance = 0
+balances = {}
 for line in lines:
     # if line == "":
     #     continue
@@ -10,8 +10,11 @@ for line in lines:
 
     parts = line.split()
     cmd = parts[0]
+    user = parts[1]
     if cmd == "DEPOSIT":
-        amount = int(parts[1])
-        balance = amount + balance
+        amount = int(parts[2])
+        # TODO: balancesを更新
+        balances[user] = balances.get(user, 0) + amount
     elif cmd == "BALANCE":
-        print(balance)
+        # TODO: userの残高を出力
+        print(balances.get(user, 0))
